d72e816c87f420c9c75ceedcf7e18cdea2037e34
V.Santana <drake.santana25@outlook.es>
2024-04-07 21:40:06 -0500

    Java

---
+++ src/main/java/co/com/ps/b1/Main.java
@@ -0,0 +1,17 @@
+package co.com.ps.b1;
+
+import java.util.Date;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int edad=30;
+        double fracciones =1.2;
+        boolean esVerdad=true;
+        short numeros= 60;
+        char letra= 'a';
+
+
+    }
+
+}
